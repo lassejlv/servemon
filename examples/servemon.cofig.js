5ade6adac016723f2a36cf8fa6dcc155ed8a4417
lassev05 <vestergaardlasse2@gmail.com>
2022-07-23 23:12:15 +0200

    Delete html file

--- examples/servemon.cofig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: 3000, // Set your custom port (Default: 3000)
-    directory: "./", // The directory to serve. (Default: current directory)
-    watch: false, // Watch the directory for changes. (Default: true)
-};
